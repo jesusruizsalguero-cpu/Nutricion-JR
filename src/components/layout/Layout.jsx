576bddaba2137c385620ad2c80ada575369f5e4d
jesusruizsalguero-cpu <jesusruizsalguero@gmail.com>
2026-09-09 00:44:18 +0200

    Revert: elimina panel de uso de API

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom'
 import BarraLateral from '@/components/layout/BarraLateral'
 import BarraInferior from '@/components/layout/BarraInferior'
 import Encabezado from '@/components/layout/Encabezado'
-import { UsageCard } from '@/components/UsageCard'
 
 /** Escritorio: barra lateral fija. Móvil: encabezado + navegación inferior. */
 export default function Layout() {
@@ -19,7 +18,6 @@ export default function Layout() {
       </div>
 
       <BarraInferior />
-      <UsageCard />
     </div>
   )
 }
